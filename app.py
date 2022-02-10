2b6194f20e056472182db1ec7d4810d5e6841f18
brunomotadev <brunomotadev@gmail.com>
2022-02-10 12:37:22 -0300

    alterado as variaveis

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import flask
 from flask import request, json, jsonify
 import requests
 
-app = flask.Flask(_name_)
+app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/", methods=["GET"])
@@ -10,5 +10,5 @@ def index():
     data = requests.get('https://randomuser.me/api')
     return data.json()
 
-if _name_ == "_main_"
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port="5000")
