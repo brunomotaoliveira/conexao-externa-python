48f440f9e3b2dd5c53bc00e28003d5e632d303c2
brunomotadev <brunomotadev@gmail.com>
2022-02-11 17:30:21 -0300

    configurado arquivo para testar yaml valido

--- 4_YAML/app.py
+++ 4_YAML/app.py
@@ -3,7 +3,7 @@ import yaml
 if __name__ == '__main__':
 
     stream = open("test.yaml", "r")
-    dictionary = yaml.safe._load(stream)
+    dictionary = yaml.safe_load(stream)
 
-    for key, value in dictionary.items();
-        print(key: " : " + str(value))
+    for key, value in dictionary.items():
+        print(key + " : " + str(value))
